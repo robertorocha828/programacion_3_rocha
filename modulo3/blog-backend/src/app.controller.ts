0813ec37096bc3c2da90f07a9219ffce0666e2a8
robertorocha828 <robertorocha828@gmail.com>
2026-05-14 09:00:36 -0500

    Clase 14-05-2026

--- modulo3/blog-backend/src/app.controller.ts
+++ modulo3/blog-backend/src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProductDto } from './product.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/health")
+  getHeath(): any {
+    return this.appService.getHeath();
   }
+
+  @Post("/products")
+  createProducto(@Body() product:ProductDto): ProductDto{
+    return this.appService.createProducto(product);
+  }
+
+  @Get("/products")
+  findAll(): ProductDto[]{
+    return this.appService.findAll();
+  }
+
+  @Get("/products")
+  findById(@Param('id') id:string): ProductDto[]{
+    return this.appService.findById(id);
+  }
+
+
 }
+
+
--- modulo3/blog-backend/src/app.service.ts
+++ modulo3/blog-backend/src/app.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { ProductDto } from './product.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private products: ProductDto[]=[
+    
+  ]
+
+  getHeath(): any {
+    return {
+      "status":"online",
+      "services" : "blog service api",
+      "version":"0.0.1",
+      "date": new Date()
+
+    }
   }
+
+ createProducto(product:ProductDto): ProductDto {
+    const newProduct: ProductDto={
+      id: Math.floor(Math.random()*1000),
+      ...product
+    }
+    this.products.push(newProduct)
+    return {
+      "id":product.id,
+      "name":product.name,
+      "price":product.price,
+      "stock": 10
+
+    }
+  }
+
+  findAll():ProductDto[]{
+    return this.products
+  }
+
+  findById(id:string):ProductDto{
+    return this.products!
+        .find(product=>product.id===Number(id))!;
+  }
+
 }
